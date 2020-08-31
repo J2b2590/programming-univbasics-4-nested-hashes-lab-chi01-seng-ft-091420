da6e80f97d819a000e87f4fff0172de7f66a68ae
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-31 20:10:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,7 +119,7 @@ def changing_alan
       }
     }
 
-		puts programmer_hash[:alan_kay][:known_for] = [:alans_new_info]
+		puts programmer_hash[:alan_kay][:known_for] = "GUI"
 		programmer_hash
 end
 
